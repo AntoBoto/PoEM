bitcoin/bitcoin

qt: use QChar(0x03BC) for mu symbol
Embedding UTF-8 in source code is not portable.

Also make unit descriptions translatable.

src/qt/bitcoinunits.cpp
@@ -41,7 +41,7 @@ QString BitcoinUnits::name(int unit)
     {
     case BTC: return QString("BTC");
     case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case uBTC: return QChar(0x03BC) + QString("BTC");
     default: return QString("???");
     }
 }
@@ -50,9 +50,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1,000,000)");
+    case BTC: return tr("Bitcoins");
+    case mBTC: return tr("Milli-Bitcoins (1 / 1,000)");
+    case uBTC: return tr("Micro-Bitcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }

AntoBoto/PoEM

src/qt/bitcoinunits.cpp
@@ -42,7 +42,7 @@ QString BitcoinUnits::name(int unit)
     {
     case BTC: return QString("DOGE");
     case mBTC: return QString("mDOGE");
-    case uBTC: return QString::fromUtf8("μDOGE");
+    case uBTC: return QChar(0x03BC) + QString("DOGE");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Dogecoins");
-    case mBTC: return QString("Milli-Dogecoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Dogecoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return tr("Dogecoins");
+    case mBTC: return tr("Milli-Dogecoins (1 / 1,000)");
+    case uBTC: return tr("Micro-Dogecoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }